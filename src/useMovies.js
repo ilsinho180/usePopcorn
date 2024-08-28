b158bc0f8f21f48390b8de39cfc186e4ff1aa9c9
Raul <simion.iles@yahoo.com>
2024-08-28 23:47:57 +0300

    Update key env

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-const KEY = "afb28ece";
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  console.log(process.env.KEY);
 
   useEffect(
     function () {
@@ -14,7 +14,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `http://www.omdbapi.com/?apikey=${process.env.KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) {
